39f23cee24917b99d740f558f11a493aaf64b8e2
kido <neoboris12@gmail.com>
2016-10-13 11:44:14 +0900

    複数人対応(描画と位置取得を分ける)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -22,51 +22,59 @@ app.controller('MainController', ['$scope', '$interval', function($scope, $inter
             ctx.drawImage(img, 0, 0, iwidth, iheight);
         }
     };
-    var users = null;
+    var mappingInfos = [];
     {
         var User = ncmb.DataStore("User");
         User.fetchAll().then(function(results) {
-            users = results;
+            for (var i = 0; i < results.length; i++) {
+                mappingInfos.push({user: results[i], locations: null});
+            }
         });
     }
     $scope.toilet = false;
     var t = $interval(function() {
-        if (users === null) { return; }
         if (layer2.getContext) {
-            var ctx = layer2.getContext('2d');
-            ctx.clearRect(0, 0, iwidth, iheight);
-            for (var u = 0; u < users.length; u++) {
-                mapping(u, ctx);
+            for (var i = 0; i < mappingInfos.length; i++) {
+                getLocation(i);
             }
         }
     }, 1000);
-    var mapping = function(u, ctx) {
-        var Location = ncmb.DataStore("Location");
-        Location.limit(5).equalTo("uuid", users[u].uuid).order("createDate", true).fetchAll().then(function(results) {
-            var r = users[u].R;
-            var g = users[u].G;
-            var b = users[u].B;
-            $scope.x = results[0].x;
-            $scope.y = results[0].y;
-            var num = 5;
-            var alpharatio = 1.0 / (num + 1);
-            for (var i = 0; i < num; i++) {
-                if (results.length <= i) {
-                    break;
-                }
-                var x = results[i].x;
-                var y = results[i].y;
-                ratiox = x * ratiowidth;
-                ratioy = y * ratioheight;
-                ctx.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + (1.0 - alpharatio * i) + ')';
-                ctx.beginPath();
-                ctx.arc(ratiox, ratioy, (num + 2 - i), 0, Math.PI * 2, true);
-                ctx.fill();
-                if (i === 0) {
-                    ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
-                    ctx.fillText(users[u].char, ratiox - 3, ratioy + 3);
+    var t_draw = $interval(function() {
+        if (layer2.getContext) {
+            var ctx = layer2.getContext('2d');
+            ctx.clearRect(0, 0, iwidth, iheight);
+            for (var i = 0; i < mappingInfos.length; i++) {
+                var locations = mappingInfos[i].locations;
+                if (locations !== null) {
+                    var user = mappingInfos[i].user;
+                    var r = user.R;
+                    var g = user.G;
+                    var b = user.B;
+                    var num = 5;
+                    var alpharatio = 1.0 / (num + 1);
+                    for (var j = 0; j < num; j++) {
+                        if (locations.length <= j) { break; }
+                        var x = locations[j].x;
+                        var y = locations[j].y;
+                        ratiox = x * ratiowidth;
+                        ratioy = y * ratioheight;
+                        ctx.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + (1.0 - alpharatio * j) + ')';
+                        ctx.beginPath();
+                        ctx.arc(ratiox, ratioy, (num + 2 - j), 0, Math.PI * 2, true);
+                        ctx.fill();
+                        if (j === 0) {
+                            ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
+                            ctx.fillText(user.char, ratiox - 3, ratioy + 3);
+                        }
+                    }
                 }
             }
+        }
+    });
+    var getLocation = function(i) {
+        var Location = ncmb.DataStore("Location");
+        Location.limit(5).equalTo("uuid", mappingInfos[i].user.uuid).order("createDate", true).fetchAll().then(function(results) {
+            mappingInfos[i].locations = results;
         }).catch(function(err) {
             $scope.err = err;
         });
@@ -74,5 +82,6 @@ app.controller('MainController', ['$scope', '$interval', function($scope, $inter
 
     $scope.onclick = function() {
         $interval.cancel(t);
+        $interval.cancel(t_draw);
     };
 }]);
